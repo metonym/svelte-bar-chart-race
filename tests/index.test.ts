cc97bb2c48102b597431a21a94e41a8c83dc4c01
metonym <ericyl.us@gmail.com>
2023-12-17 15:36:18 -0800

    test: simplify package test suite

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,22 +1,20 @@
-import { describe, expect, it } from "vitest";
+import { expect, it } from "vitest";
 import * as pkg from "../package.json";
 import * as API from "../src";
 
-describe(pkg.name, () => {
-  it("has 0 dependencies", () => {
-    // @ts-expect-error
-    expect(Object.keys(pkg.dependencies ?? {}).length).toEqual(0);
-  });
+it("has 0 dependencies", () => {
+  // @ts-expect-error
+  expect(Object.keys(pkg.dependencies ?? {}).length).toEqual(0);
+});
 
-  it("has named exports", () => {
-    // @ts-expect-error
-    expect(API.default).toBeUndefined();
-    expect(Object.keys(API)).toMatchInlineSnapshot(`
-        [
-          "BarChartRace",
-          "Chart",
-          "Slider",
-        ]
-      `);
-  });
+it("has named exports", () => {
+  // @ts-expect-error
+  expect(API.default).toBeUndefined();
+  expect(Object.keys(API)).toMatchInlineSnapshot(`
+      [
+        "BarChartRace",
+        "Chart",
+        "Slider",
+      ]
+    `);
 });
