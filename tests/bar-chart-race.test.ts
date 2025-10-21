12362463eb756d36de38ae891b327df90e348401
Eric Liu <ericyl.us@gmail.com>
2025-10-21 11:21:13 -0700

    chore(deps-dev): upgrade `@testing-library/svelte`

--- tests/bar-chart-race.test.ts
+++ tests/bar-chart-race.test.ts
@@ -20,7 +20,7 @@ describe("BarChartRace", () => {
 
     expect(target.querySelectorAll("li").length).toEqual(5);
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `Rust: 73.1% <svg width="100%" style=\"\"><rect height="100%" width="73.1%" fill="#dea584"></rect></svg> `,
+      `Rust: 73.1% <svg width="100%"><rect height="100%" width="73.1%" fill="#dea584"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("2017");
   });
@@ -34,7 +34,7 @@ describe("BarChartRace", () => {
 
     expect(target.querySelectorAll("li").length).toEqual(5);
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `Rust: 78.9% <svg width="100%" style=\"\"><rect height="100%" width="78.9%" fill="#dea584"></rect></svg> `,
+      `Rust: 78.9% <svg width="100%"><rect height="100%" width="78.9%" fill="#dea584"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("2018");
     expect(target.querySelector("strong")!.innerHTML).toEqual("2018");
@@ -42,7 +42,7 @@ describe("BarChartRace", () => {
     await userEvent.click(target.querySelectorAll("button")[0]);
 
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `Rust: 86.1% <svg width=\"100%\" style=\"\"><rect height=\"100%\" fill=\"#dea584\" width=\"86.1%\"></rect></svg> `,
+      `Rust: 86.1% <svg width=\"100%\"><rect height=\"100%\" width=\"86.1%\" fill=\"#dea584\"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("2020");
     expect(target.querySelector("strong")!.innerHTML).toEqual("2020");
@@ -50,7 +50,7 @@ describe("BarChartRace", () => {
     await userEvent.click(target.querySelectorAll("button")[1]);
 
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `Rust: 73.1% <svg width=\"100%\" style=\"\"><rect height=\"100%\" fill=\"#dea584\" width=\"73.1%\"></rect></svg> `,
+      `Rust: 73.1% <svg width=\"100%\"><rect height=\"100%\" width=\"73.1%\" fill=\"#dea584\"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("2017");
     expect(target.querySelector("strong")!.innerHTML).toEqual("null");
@@ -65,28 +65,28 @@ describe("BarChartRace", () => {
 
     expect(target.querySelectorAll("li").length).toEqual(5);
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `<strong>Rust</strong> 73.1% <svg width="100%" style=\"\"><rect height="100%" width="73.1%" fill="#dea584"></rect></svg> `,
+      `<strong>Rust</strong> 73.1% <svg width="100%"><rect height="100%" width="73.1%" fill="#dea584"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("Year: 2017");
 
     await userEvent.click(target.querySelectorAll("button")[1]);
 
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `<strong>Rust</strong> 87.0% <svg width="100%" style=\"\"><rect height="100%" fill=\"#dea584\" width="86.98%"></rect></svg> `,
+      `<strong>Rust</strong> 87.0% <svg width="100%"><rect height="100%" width="86.98%" fill=\"#dea584\"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("Year: 2021");
 
     await userEvent.click(target.querySelectorAll("button")[2]);
 
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `<strong>Rust</strong> 73.1% <svg width="100%" style=\"\"><rect height="100%" fill="#dea584" width="73.1%"></rect></svg> `,
+      `<strong>Rust</strong> 73.1% <svg width="100%"><rect height="100%" width="73.1%" fill="#dea584"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("Year: 2017");
 
     await userEvent.click(target.querySelectorAll("button")[3]);
 
     expect(target.querySelectorAll("li")[0].innerHTML).toEqual(
-      `<strong>Rust</strong> 83.5% <svg width=\"100%\" style=\"\"><rect height=\"100%\" fill=\"#dea584\" width=\"83.5%\"></rect></svg> `,
+      `<strong>Rust</strong> 83.5% <svg width=\"100%\"><rect height=\"100%\" width=\"83.5%\" fill=\"#dea584\"></rect></svg> `,
     );
     expect(target.querySelector("label")!.innerHTML).toEqual("Year: 2019");
   });
